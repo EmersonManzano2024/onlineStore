0e7b241d2f1654463c07687e0811806c38168ecd
Noe Manzano <noemanzano998@gmail.com>
2023-10-16 11:19:07 -0600

    New

--- Pagina2/JS/app.js
+++ Pagina2/JS/app.js
@@ -36,7 +36,7 @@ stockProductos.forEach((producto) => {
     <img src=${producto.img} alt= "">
     <h3>${producto.nombre}</h3>
     <p>${producto.desc}</p>
-    <p>Talle: ${producto.talle}</p>
+    <p>Tipo: ${producto.talle}</p>
     <p class="precioProducto">Precio:$ ${producto.precio}</p>
     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
 
